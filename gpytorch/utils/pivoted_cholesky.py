b1cc4f99cb6bf720383ed9b0ebf1bcbf254520c8
Jake Gardner <jrg365@cornell.edu>
2018-03-13 15:48:22 -0400

    Add code for using Woodbury to do solves with pivoted Cholesky factors.

--- gpytorch/utils/pivoted_cholesky.py
+++ gpytorch/utils/pivoted_cholesky.py
@@ -34,3 +34,37 @@ def pivoted_cholesky(matrix, max_iter, error_tol=1e-5):
         m = m + 1
 
     return L[:m, :]
+
+
+def woodbury_factor(low_rank_mat, shift):
+    """
+    Given a k-by-n matrix V and a shift \sigma, computes R so that
+    R'R = V'(I_k + 1/\sigma VV')^{-1}V
+    """
+    n = low_rank_mat.size(-1)
+    k = low_rank_mat.size(-2)
+    shifted_mat = (1 / shift) * low_rank_mat.matmul(low_rank_mat.t())
+    shifted_mat = shifted_mat + shifted_mat.new(k).fill_(1).diag()
+    # P'P = I_k + 1/\sigma VV'
+    # Therefore, V'(I_k + 1/\sigma VV')V = V'P^{-1}P^{-T}V
+    cholesky_factor = torch.potrf(shifted_mat)
+    # R = P^{-T}V
+    R = torch.trtrs(low_rank_mat, cholesky_factor.t(), upper=False)[0]
+
+    return R
+
+
+def woodbury_solve(vector, woodbury_factor, shift):
+    """
+    Solves the system of equations:
+        (sigma*I + VV')x = b
+    Using the Woodbury formula.
+
+    Input:
+        - vector (size n) - right hand side vector b to solve with.
+        - woodbury_factor (k x n) - The result of calling woodbury_factor on V
+          and the shift, \sigma
+        - shift (scalar) - shift value sigma
+    """
+    right = (1 / shift) * woodbury_factor.t().matmul(woodbury_factor.matmul(vector))
+    return (1 / shift) * (vector - right)
