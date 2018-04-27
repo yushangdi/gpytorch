1c3fca0e9416806b94e48e39ef56c3067cc0f79c
Geoff Pleiss <gpleiss@gmail.com>
2018-04-27 15:25:09 -0400

    Make it fast

--- gpytorch/utils/batched_tri_to_diag.py
+++ gpytorch/utils/batched_tri_to_diag.py
@@ -154,7 +154,7 @@ def batched_tridiag_to_diag2(t_mat):
         eigenvalues[:,:,0] = mat[:,:,0,0]
     return eigenvalues, eigenvectors
 
-def batched_tridiag_to_diag(a, b):
+def batched_tridiag_to_diag(a, b, err=1e-5, eps=1e-30):
     """
     Given a num_init_vecs*num_batch by k batched vector/matrix alpha
     and a num_init_vecs*num_batch by k-1 batched vector/matrix beta,
@@ -163,50 +163,100 @@ def batched_tridiag_to_diag(a, b):
     Computes symmetric tridiagonal QR algorithm with implicit Wilkinson shift
     as described in http://people.inf.ethz.ch/arbenz/ewp/Lnotes/chapter4.pdf.
     """
-    n1n2 = a.size(0)
-    n3 = a.size(1)
+    a_trsp = a.t().contiguous()
+    b_trsp = b.t().contiguous()
+
+    n3, n1n2 = a_trsp.size()
 
     m = n3 - 1
-    eigenvalues = torch.zeros(n1n2, n3)
-    eigenvalues = a.new(n1n2, n3).fill_(0)
-    eigenvectors = a.new(n3).fill_(1).diag().repeat(n1n2, 1, 1)
-    err = 10 ** -6
+    eigenvalues = a_trsp.new(n3, n1n2).fill_(0)
+    eigenvectors = (
+        a_trsp.new(n3).fill_(1).
+        diag().
+        repeat(n1n2, 1, 1).
+        permute(2, 0, 1).contiguous()
+    )
 
-    c = a.new(n1n2, 1)
-    s = c.clone()
-    d = c.clone()
+    c = a_trsp.new(n1n2)
+    s = a_trsp.new(n1n2)
+    c2_minus_s2 = a_trsp.new(n1n2)
+
+    d = a_trsp.new(n1n2)
+    w = a_trsp.new(n1n2)
+    am1 = a_trsp.new(n1n2)
+    bm = a_trsp.new(n1n2)
+
+    x = a_trsp.new(n1n2)
+    y = a_trsp.new(n1n2)
+    z = a_trsp.new(n1n2)
+    x2 = a_trsp.new(n1n2)
+    y2 = a_trsp.new(n1n2)
+
+    bk_abs = a_trsp.new(n1n2)
+    vecs1 = a_trsp.new(n1n2, n3)
+
     while (m > 0):
-        s = a[:, m].clone()
-        am1 = a[:, m - 1].clone()
-        bm = b[:, m - 1].clone().pow_(2)
-        d = am1.sub_(s) / 2  # Computes Wilkinson's shift
-        s.sub_(bm.div_(torch.sign(d.add_(1e-30)).mul_((torch.pow(d, 2).add_(bm)).sqrt_()).add_(d)))
-        x = (-s).add_(a[:, 0]) # Implicit QR
-        y = b[:, 0]
-        for k in range(0,m):
-            c = (torch.pow(x, 2).add_(torch.pow(y, 2))).rsqrt_()
-            s = c.clone()
+        s.copy_(a_trsp[m])
+        am1.copy_(a_trsp[m - 1])
+        bm.copy_(b_trsp[m - 1]).pow_(2)
+
+        # d = (am1 - s) / 2
+        torch.sub(am1, s, out=d).div_(2)  # Computes Wilkinson's shift
+        # Come back
+        s.sub_(
+            bm.div_(
+                torch.sign(d.add_(eps)).
+                mul_((torch.pow(d, 2).add_(bm)).sqrt_()).
+                add_(d)
+            )
+        )
+
+        # Come back
+        x.copy_(s).mul_(-1).add_(a_trsp[0]) # Implicit QR
+        y.copy_(b_trsp[0])
+        for k in range(0, m):
+            torch.pow(x, 2, out=x2)
+            torch.pow(y, 2, out=y2)
+            torch.add(x2, y2, out=c).rsqrt_()
+
+            s.copy_(c)
             c.mul_(x)
             s.mul_(-y)
-            w = (torch.mul(c, x)).sub_(torch.mul(s, y))
-            d = a[:, k] - a[:, k + 1]
-            z = ((torch.mul(d, s)).addcmul_(2, c, b[:, k])).mul_(s)
-            a[:, k] -= z
-            a[:, k + 1] += z
-            (b[:, k].mul_(torch.pow(c, 2).sub_(torch.pow(s, 2)))).add_(d.mul_(torch.mul(c, s)))
-            x = b[:, k]
+
+            torch.mul(c, x, out=w).addcmul_(-1, s, y)
+            torch.sub(a_trsp[k], a_trsp[k + 1], out=d)
+            torch.mul(d, s, out=z).addcmul_(2, c, b_trsp[k]).mul_(s)
+
+            # c ** 2 - s ** 2
+            torch.pow(c, 2, out=c2_minus_s2)
+            c2_minus_s2.addcmul_(-1, s, s)
+
+            # d <- d * c * s
+            d.mul_(c).mul_(s)
+
+            a_trsp[k] -= z
+            a_trsp[k + 1] += z
+            b_trsp[k].mul_(c2_minus_s2).add_(d)
+
+            x.copy_(b_trsp[k])
             if k > 0:
-                b[:, k - 1] = w
+                b_trsp[k - 1].copy_(w)
             if k < m - 1:
-                y = (-s).mul_(b[:, k + 1])
-                b[:, k + 1].mul_(c)
+                y.copy_(s).mul_(-1).mul_(b_trsp[k + 1])
+                b_trsp[k + 1].mul_(c)
             c.unsqueeze_(-1)
             s.unsqueeze_(-1)
-            vecs1 = eigenvectors[:, :, k].clone()
-            (eigenvectors[:, :, k].mul_(c)).addcmul_(-1, eigenvectors[:, :, k + 1], s)
-            (eigenvectors[:, :, k + 1].mul_(c)).addcmul_(vecs1, s)
-        if torch.norm(b[:, k]) < err:
-            eigenvalues[:, m] = a[:, m]
+
+            vecs1.copy_(eigenvectors[k])
+            eigenvectors[k].mul_(c).addcmul_(-1, eigenvectors[k + 1], s)
+            eigenvectors[k + 1].mul_(c).addcmul_(vecs1, s)
+            c.squeeze_(-1)
+            s.squeeze_(-1)
+
+        torch.abs(b_trsp[k], out=bk_abs)
+        if bk_abs.max() < err:
+            eigenvalues[m].copy_(a_trsp[m])
             m -= 1
-    eigenvalues[:, 0] = a[:, 0]
-    return eigenvalues, eigenvectors
+
+    eigenvalues[0].copy_(a_trsp[0])
+    return eigenvalues.t().contiguous(), eigenvectors.permute(1, 2, 0).contiguous()
--- test/util/test_batched.py
+++ test/util/test_batched.py
@@ -1,11 +1,14 @@
+import os
 import torch
 import unittest
 from gpytorch.utils.batched_tri_to_diag import batched_tridiag_to_diag, lanczos_tridiag_to_diag
 
 
 class TestBatched(unittest.TestCase):
-    def test_vectorized_eigenvalues(self):
-        t_mat = torch.load('actual_t_mats.pth')
+    def setUp(self):
+        super(TestBatched, self).__init__()
+        dirname = os.path.dirname(os.path.realpath(__file__))
+        t_mat = torch.load(os.path.join(dirname, 'actual_t_mats.pth'))
         sym_mat = t_mat.clone()
         b1 = t_mat.size(0)
         b2 = t_mat.size(1)
@@ -20,33 +23,51 @@ class TestBatched(unittest.TestCase):
             for j in range(b2):
                 beta[i, j] = t_mat[i, j, :, :].diag(-1)
         beta.resize_(b1 * b2, b3 - 1)
-        val, vec = batched_tridiag_to_diag(alpha.clone(), beta.clone())
-        sym_val, sym_vec = lanczos_tridiag_to_diag(sym_mat.clone())
 
-        val.resize_(b1, b2, b3)
+        self.alpha = alpha
+        self.beta = beta
+        self.sym_mat = sym_mat
+        self.b1 = b1
+        self.b2 = b2
+        self.b3 = b3
+
+    def test_vectorized_eigenvalues(self):
+        val, vec = batched_tridiag_to_diag(self.alpha.clone(), self.beta.clone())
+        sym_val, sym_vec = lanczos_tridiag_to_diag(self.sym_mat.clone())
+
+        val.resize_(self.b1, self.b2, self.b3)
         res = torch.norm(torch.sort(sym_val)[0] - torch.sort(val)[0])
         self.assertLess(res, 1e-3)
 
-    def test_vectorized_eigenvectors(self):
-        t_mat = torch.load('actual_t_mats.pth')
-        sym_mat = t_mat.clone()
-        b1 = t_mat.size(0)
-        b2 = t_mat.size(1)
-        b3 = t_mat.size(2)
-        alpha = torch.zeros(b1, b2, b3)
-        for i in range(b1):
-            for j in range(b2):
-                alpha[i, j] = t_mat[i, j, :, :].diag()
-        alpha.resize_(b1 * b2, b3)
-        beta = torch.zeros(b1, b2, b3 - 1)
-        for i in range(b1):
-            for j in range(b2):
-                beta[i, j] = t_mat[i, j, :, :].diag(-1)
-        beta.resize_(b1 * b2, b3 - 1)
-        val, vec = batched_tridiag_to_diag(alpha.clone(), beta.clone())
-        sym_val, sym_vec = lanczos_tridiag_to_diag(sym_mat.clone())
+    def test_timing(self):
+        import time
+        n_iter = 20
 
-        vec.resize_(b1, b2, b3, b3)
+        alpha = self.alpha.cuda()
+        beta = self.beta.cuda()
+        sym_mat = self.sym_mat.cuda()
+        alpha = alpha + 1 - 1
+        beta = beta + 1 - 1
+        sym_mat = sym_mat + 1 - 1
+
+        start = time.time()
+        orig_alpha = alpha.clone()
+        for i in range(n_iter):
+            val, vec = batched_tridiag_to_diag(alpha, beta)
+        res = time.time() - start
+        print('Batched time: %.4f' % (res / n_iter))
+
+        start = time.time()
+        for i in range(n_iter):
+            sym_val, sym_vec = lanczos_tridiag_to_diag(sym_mat)
+        res = time.time() - start
+        print('Normal time: %.4f' % (res / n_iter))
+
+    def test_vectorized_eigenvectors(self):
+        val, vec = batched_tridiag_to_diag(self.alpha.clone(), self.beta.clone())
+        sym_val, sym_vec = lanczos_tridiag_to_diag(self.sym_mat.clone())
+
+        vec.resize_(self.b1, self.b2, self.b3, self.b3)
         res = torch.norm(torch.sort(torch.abs(sym_vec), -1)[0] - torch.sort(torch.abs(vec), -1)[0])
         self.assertLess(res, 1e-3)
 
